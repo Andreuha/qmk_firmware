4479c440b63f635f0c953debffd6357afc5dabc7
u/andylikescandy <andylikescandy@gmail.com>
2018-08-05 09:54:44 -0700

    Updated name and keymaps

--- keyboards/handwired/lap50/keymaps/default/keymap.c
+++ keyboards/handwired/lap50/keymaps/default/keymap.c
@@ -1,19 +1,11 @@
 #include "lap50.h"
 
-
-// Key Combos
-#define CTRLSFT LCTL(KC_LSFT)
-#define CTLALTSFT LALT(LCTL(KC_LSFT))
-#define CTLALTDEL LALT(LCTL(KC_DEL))
-#define LTSYMWIN LT(2,KC_LGUI)
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = KEYMAP( /* lap50 - QWERTY */
       KC_ESC,   KC_1,     KC_2,     KC_3,    KC_4,     KC_5,     KC_6,     KC_7,    KC_8,     KC_9,     KC_0,     KC_DEL,  \
       KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,     KC_O,     KC_P,     KC_BSPC,  \
       KC_CAPS,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,     KC_J,    KC_K,     KC_L,               KC_ENT,   \
-      KC_LSFT,            KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,     KC_N,     KC_M,    KC_COMM,  SFT_T(KC_DOT),SFT_T(KC_SLSH),  \
+      KC_LSFT,            KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,     KC_N,     KC_M,    KC_COMM,  SFT_T(KC_DOT),KC_SLSH,  \
       KC_LCTL,  KC_LGUI,  KC_LALT,                     LT(1,KC_SPC),                                    KC_RALT,  MO(2) \
   ),
   [1] = KEYMAP( /* lap50 - Arrows */
@@ -21,13 +13,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   XXXXXXX,  KC_UP,     XXXXXXX,    XXXXXXX,     XXXXXXX,     KC_HOME,    KC_PGDN,    KC_PGUP,     KC_END,      KC_MINS,   KC_EQL,  \
       XXXXXXX,  KC_LEFT,  KC_DOWN,   KC_RIGHT,   XXXXXXX,     XXXXXXX,     KC_LEFT,    KC_DOWN,    KC_UP,       KC_RIGHT,               _______,     \
       _______,            XXXXXXX,   XXXXXXX,    XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,    XXXXXXX,    XXXXXXX,     _______,    XXXXXXX,  \
-      _______,  KC_LGUI,  _______,                     _______,                                                              CTRLSFT,    _______ \
+      _______,  _______,  _______,                     _______,                                                              _______,    _______ \
   ),
   [2] = KEYMAP( /* lap50 - Empty */
-      _______,  KC_F11,   KC_F12,    XXXXXXX,    XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX,    CTLALTDEL,  \
+      _______,  KC_F11,   KC_F12,    XXXXXXX,    XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,    XXXXXXX,    XXXXXXX,     XXXXXXX,    _______,  \
       _______,  XXXXXXX,  XXXXXXX,   XXXXXXX,    XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,    XXXXXXX,    KC_LBRC,     KC_RBRC,    KC_BSLS,  \
-      _______,  XXXXXXX,  KC_PSCREEN,XXXXXXX,    XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,    KC_SCLN,    KC_QUOT,                 _______,   \
-      _______,            XXXXXXX,   KC_PAUSE,   KC_SLCK,     KC_INSERT,     XXXXXXX,     XXXXXXX,    XXXXXXX,    LSFT(KC_COMM),SFT_T(LSFT(KC_DOT)),LSFT(KC_SLSH),  \
+      _______,  XXXXXXX,  XXXXXXX,   XXXXXXX,    XXXXXXX,     XXXXXXX,     XXXXXXX,    XXXXXXX,    XXXXXXX,     KC_SCLN,                 KC_QUOT,   \
+      _______,            XXXXXXX,   XXXXXXX,    XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,    XXXXXXX,    LSFT(KC_COMM),SFT_T(SFT_T(KC_DOT)),LSFT(KC_SLSH),  \
       _______,  _______,  _______,                     _______,                                                              _______,    _______ \
   ),
   [9] = KEYMAP( /* lap50 - Empty */
