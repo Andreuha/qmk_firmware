d0d31c510e4f99e7e29f1da591d86656b7508ee9
andrew-terentyev <andrew.terentyev@ipreo.com>
2018-10-03 02:39:51 -0400

    Fixed tapping time and added the source for the OLED image

--- keyboards/helix/rev2/keymaps/andylikescandy/config.h
+++ keyboards/helix/rev2/keymaps/andylikescandy/config.h
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* period of tapping(ms) */
 #ifndef TAPPING_TERM
-#define TAPPING_TERM    3000
+#define TAPPING_TERM    300
 #endif
 
 
@@ -34,6 +34,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
 #define PREVENT_STUCK_MODIFIERS
-#define QMK_KEYS_PER_SCAN 2
+//#define QMK_KEYS_PER_SCAN 1
 
 #endif /* CONFIG_USER_H */
